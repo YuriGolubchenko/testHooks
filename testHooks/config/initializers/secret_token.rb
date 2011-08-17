bfd69037f03bdaec9858b886ec7bea0e21f6692e
Yuri Golubchenko <yurig@yurig-pc.(none)>
2011-08-17 15:38:01 +0300

    first commit

---
+++ testHooks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestHooks::Application.config.secret_token = '75fefe6c91854ab7f9da41a82f0baa10afd2217d686ed06ea384f5bc2aabb66d04de43a4373bdc73987dc3f18449741b2799720eb2e7ea1bf737a439e6e9b453'
